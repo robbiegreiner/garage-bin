469659fd87f10d2baec028af1e7e9bdfd85fda6d
Robbie Greiner <robbieg@gmail.com>
2017-12-18 10:33:38 -0700

    showing items on page

--- public/index.js
+++ public/index.js
@@ -1,8 +1,19 @@
+
+const showItems = (items) => {
+  items.forEach(item => {
+    $('.items-container').append(`
+      <div class='item${item.id} item'>
+        <h2 class='item-name'>${item.name}</h2>
+      </div>
+    `);
+  });
+};
+
 const getItems = () => {
   fetch('/api/v1/items')
     .then(response => response.json())
     .then(items => {
-      console.log(items);
+      showItems(items);
     });
 };
 
